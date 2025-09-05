138359fef7ae4d81f2f2842867089def68e67e8c
Mark Wolfman <canismarko@gmail.com>
2025-09-05 09:26:56 -0500

    Added a landing page.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,32 +1,46 @@
 import './App.css';
 import Navbar from "./navbar.tsx";
+import { Link } from "react-router";
 
 function App() {
 
     return (
-	<>
-	  <Navbar />
-          {/* <div> */}
-          {/*   <a href="https://vite.dev" target="_blank"> */}
-          {/*     <img src={viteLogo} className="logo" alt="Vite logo" /> */}
-          {/*   </a> */}
-          {/*   <a href="https://react.dev" target="_blank"> */}
-          {/*     <img src={reactLogo} className="logo react" alt="React logo" /> */}
-          {/*   </a> */}
-          {/* </div> */}
-          {/* <h1>Vite + React</h1> */}
-          {/* <div className="card"> */}
-          {/*   <button onClick={() => setCount((count) => count + 1)}> */}
-          {/*     count is {count} */}
-          {/*   </button> */}
-          {/*   <p> */}
-          {/*     Edit <code>src/App.tsx</code> and save to test HMR */}
-          {/*   </p> */}
-          {/* </div> */}
-          {/* <p className="read-the-docs"> */}
-          {/*   Click on the Vite and React logos to learn more */}
-          {/* </p> */}
-        </>
+	<div className="w-full mx-auto max-w-7xl pt-6">
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">Hello, Spectroscopist!</h1>
+          <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">This is the internal website of the <span className="bold">Spectroscopy group</span> at the Advanced Photon Source</p>
+          <div className="grid grid-cols-2 gap-8">
+            <div className="card bg-base-100 shadow-sm m-2">
+              {/* <figure> */}
+              {/*   <img */}
+              {/*     src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" */}
+              {/*     alt="Shoes" /> */}
+              {/* </figure> */}
+              <div className="card-body">
+                <h2 className="card-title">Run Catalog</h2>
+                <p>Browse the catalog of runs measured at the spectroscopy group beamlines.</p>
+                <div className="card-actions">
+                  <Link to="/catalog" className="btn btn-primary text-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5m.75-9 3-3 2.148 2.148A12.061 12.061 0 0 1 16.5 7.605" />
+</svg>
+                    Browse
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="card bg-base-100 shadow-sm m-2">
+              <div className="card-body">
+                <h2 className="card-title">Beamline Status</h2>
+                <p>See information about the status of each beamline, such as whether data are being collected.</p>
+                <div className="card-actions">
+                  <button className="btn disabled">Coming soon…</button>
+                </div>
+              </div>
+            </div>
+
+        </div>
+        </div>
     );
 }
 
