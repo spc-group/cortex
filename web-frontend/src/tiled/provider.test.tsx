4a4d099a8d27a054194816d2ffa120919a31d45b
Mark Wolfman <canismarko@gmail.com>
2026-03-23 13:57:41 -0500

    Better parsing of Tiled server URI's.

--- web-frontend/src/tiled/provider.test.tsx
+++ web-frontend/src/tiled/provider.test.tsx
@@ -52,6 +52,16 @@ describe("the TiledProvider component", () => {
       screen.getByText("ws: ws://localhost:0/api/v1/stream/single"),
     ).toBeInTheDocument();
   });
+  it("upgrade websockets uri to wss", () => {
+    render(
+      <TiledProvider uri="https://localhost:0">
+        <MockComponent />
+      </TiledProvider>,
+    );
+    expect(
+      screen.getByText("ws: wss://localhost:0/api/v1/stream/single"),
+    ).toBeInTheDocument();
+  });
   it("provides an explicit zarr root", () => {
     const root = zarr.root(new zarr.FetchStore("http://localhost:0"));
     render(
@@ -69,6 +79,24 @@ describe("the TiledProvider component", () => {
     );
     expect(
       screen.getByText("zarr: http://localhost:0/zarr/v3"),
+    ).toBeInTheDocument();
+  });
+  it("parses the URI path", () => {
+    // We should be able to use something like
+    // http://tiled.example.com/my_group/ as a root path
+    render(
+      <TiledProvider uri="http://localhost:0/path_root/api/v1/catalog">
+        <MockComponent />
+      </TiledProvider>,
+    );
+    expect(
+      screen.getByText("tiled: http://localhost:0/path_root/api/v1"),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("ws: ws://localhost:0/path_root/api/v1/stream/single"),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("zarr: http://localhost:0/path_root/zarr/v3/catalog"),
     ).toBeInTheDocument();
   });
 });
--- web-frontend/src/tiled/provider.tsx
+++ web-frontend/src/tiled/provider.tsx
@@ -4,49 +4,78 @@ import type { ReactElement } from "react";
 import { TiledContext, ZarrRootContext, WebSocketContext } from "./context";
 import type { ZarrRoot } from "./types";
 
+const trimLeadSlash = (value: string) => {
+  return value.replace(/^\/+/, "");
+};
+
+const trimEndSlash = (value: string) => {
+  return value.replace(/\/+$/, "");
+};
+
+const trimSlashes = (value: string) => {
+  return trimLeadSlash(trimEndSlash(value));
+};
+
+// Parse a Tiled path and extract semantic segments that can be reconstructed later
+const parseUriPath = (uri: URL) => {
+  let pathRoot, catalog;
+  [pathRoot, catalog] = uri.pathname.split("/api/v1");
+  catalog = catalog == null ? catalog : trimSlashes(catalog);
+
+  // Make sure there's a trailing slash
+  pathRoot = trimEndSlash(pathRoot);
+  return {
+    pathRoot,
+    apiVersion: "1",
+    catalog,
+  };
+};
+
 // Sets up the Tiled contexts with URI's for the various parts of the Tiled server
 export const TiledProvider = ({
   uri,
   children,
-  pathPrefix,
   zarrRoot,
 }: {
   children: ReactElement[] | ReactElement;
   uri?: string;
-  pathPrefix?: string;
   zarrRoot?: ZarrRoot;
 }) => {
   // Parse the URI
-  let httpUri, wsUri;
+  let httpUri, wsUri, zarrUri, catalog, pathRoot;
   if (uri != null) {
+    let apiVersion;
+    // Parse input URI to determine the actual server URIs
     httpUri = new URL(uri);
+    ({ pathRoot, apiVersion, catalog } = parseUriPath(httpUri));
+    httpUri.pathname = trimLeadSlash(`${pathRoot}/api/v${apiVersion}`);
     wsUri = new URL(uri);
-    wsUri.protocol = "ws";
+    wsUri.protocol = httpUri.protocol === "http:" ? "ws" : "wss";
+    wsUri.pathname = `${httpUri.pathname}/stream/single`;
+    zarrUri = new URL(uri);
+    zarrUri.pathname = `${pathRoot}/zarr/v3`;
+    if (catalog) {
+      zarrUri.pathname = `${zarrUri.pathname}/${catalog}`;
+    }
   }
   // Mutliple wrapped contexts
   let element = children;
   // Set up context for regular API queries
   if (uri != null && httpUri != null && wsUri != null) {
     const tiledInfo = {
-      baseUri: `${httpUri.origin}/api/v1`,
-      pathPrefix: pathPrefix ?? "",
+      baseUri: httpUri.href,
+      pathPrefix: catalog,
     };
     element = (
       <TiledContext value={tiledInfo}>
-        <WebSocketContext value={`${wsUri.origin}/api/v1/stream/single`}>
-          {element}
-        </WebSocketContext>
+        <WebSocketContext value={wsUri.href}>{element}</WebSocketContext>
       </TiledContext>
     );
   }
   if (zarrRoot != null) {
     element = <ZarrRootContext value={zarrRoot}>{element}</ZarrRootContext>;
-  } else if (httpUri != null) {
-    let zarrUri = `${httpUri.origin}/zarr/v3`;
-    if (pathPrefix != null) {
-      zarrUri = `${zarrUri}/${pathPrefix}`;
-    }
-    const root = zarr.root(new zarr.FetchStore(zarrUri));
+  } else if (zarrUri != null) {
+    const root = zarr.root(new zarr.FetchStore(zarrUri.href));
     element = <ZarrRootContext value={root}>{element}</ZarrRootContext>;
   }
   return element;
